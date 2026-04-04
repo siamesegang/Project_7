fca9948a880fd70c98b149d65ca468a40b1ca27f
siamesegang <genarocabreraarellano@gmail.com>
2026-04-03 21:19:40 -0500

    agrega requirements, el app, un cuaderno de jupyter y actualiza el README

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Project_7
 This is a project to pass the chapter 7 of the course to become a data analyst. 
+he creado una casilla de seleccion para crear un histograma o un diagrama dedispersion de los datos proporcionados en el archivo "vehicles_us.csv"
---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv('vehicles_us.csv') #leo los datos
+st.header("Aplicacion para estudiar datos?")
+
+hist_button = st.checkbox('Construir un histograma')
+scat_button = st.checkbox("Construir un gráfico de dispersión")
+
+#hist_button = st.button('Construir histograma')
+
+if hist_button: #al hacer clic en el botón
+    #escribir un mensaje
+    st.write("Creación de un histograma para el conjunto de datos de anuncion de venta de automóviles")
+
+    #crear un histograma
+    fig = px.histogram(car_data, x="odómetro")
+
+    #mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+#scat_button = st.button('Construir gráfico de dispersión')
+
+if scat_button: #al hacer clic en el botón
+    #escribir mensaje
+    st.write("Creación de un gráficode dispersion para el conjunto de datos de anuncions de venta de automóviles")
+
+    #crear el gráfico de dispersion
+    fig =px.scatter(car_data, x="Odómetro", y="Precio")
+
+    #mostrar un gráfico plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
