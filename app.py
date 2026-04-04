75eec37cb7fc0dc6199e75fac29f86f44b99ec68
siamesegang <genarocabreraarellano@gmail.com>
2026-04-03 21:54:14 -0500

    correcion de nombres de columnas disponibles en el csv

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ if hist_button: #al hacer clic en el botón
     st.write("Creación de un histograma para el conjunto de datos de anuncion de venta de automóviles")
 
     #crear un histograma
-    fig = px.histogram(car_data, x="odómetro")
+    fig = px.histogram(car_data, x="odometer")
 
     #mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
@@ -24,10 +24,10 @@ if hist_button: #al hacer clic en el botón
 
 if scat_button: #al hacer clic en el botón
     #escribir mensaje
-    st.write("Creación de un gráficode dispersion para el conjunto de datos de anuncions de venta de automóviles")
+    st.write("Creación de un gráfico de dispersion para el conjunto de datos de anuncions de venta de automóviles")
 
     #crear el gráfico de dispersion
-    fig =px.scatter(car_data, x="Odómetro", y="Precio")
+    fig =px.scatter(car_data, x="odometer", y="price")
 
     #mostrar un gráfico plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
